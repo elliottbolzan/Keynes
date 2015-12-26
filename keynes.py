ec2cbf5549adf0ca687d25282232af379b613043
Elliott Bolzan <elliott@Elliotts-MacBook-Air.local>
2015-12-26 00:41:04 -0500

    Added some features to the trading simulation.

--- keynes.py
+++ keynes.py
@@ -1,7 +1,13 @@
 import csv, numpy, os.path, parse_data
 
 from sklearn import preprocessing
-from sklearn.feature_selection import RFE
+from sklearn import metrics
+
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import SelectKBest
+from sklearn.grid_search import GridSearchCV
+
 from sklearn import cross_validation
 from sklearn.metrics import accuracy_score
 
@@ -10,45 +16,43 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 
-def simulate(values, labels, closes):
+def simulate(trends, closes, investment):
+    base = investment
+    quote = 0
+    for x in range(0, len(trends) - 1):
+        assets = base + quote / closes[x]
+        if trends[x] == 1:
+            # Sell what you already bought.
+            base += quote / closes[x]
+            quote = 0
+        elif trends[x] == 0:
+            # Buy what's going to be worth more.
+            quote += base * closes[x]
+            base = 0
+        assets = base + quote / closes[x]
+    return assets - investment
+
+def estimate(values, labels, closes):
 
     classifier = LogisticRegression()
 
-    data = []
-    input = open(directory + "/data/hour/EURUSD_hour.csv", 'rt')
-    try:
-        reader = csv.reader(input)
-        for row in reader:
-            if "<TICKER>" not in row:
-                data.append(float(row[6]))
-    finally:
-        input.close()
-
-    bet = 18
-    resources = 1000
-
-    base = resources
-    quote = 0
-
     predicted = cross_validation.cross_val_predict(classifier, values, labels, cv = 10)
-
-    for x in range(1, len(predicted)):
-        if predicted[x] == 1:
-            base -= bet
-            quote += bet * closes[x - 1]
-        elif predicted[x] == 0:
-            base += quote / closes[x - 1]
-            quote = 0
-
-    assets = base + quote / closes[x - 1]
-    timespan = len(predicted) / 24
-
-    profit = (assets - resources) / timespan
-    percentage = profit / resources
-
-    print "Average daily profit: %0.2f euros." % profit
-    print "That's %0.4f%% a day." % percentage
     print "Model accuracy, determined through 10-fold cross-validation: %0.2f%%." % (accuracy_score(labels, predicted) * 100)
+
+    investment = 100
+    maximal = simulate(labels, closes, investment)
+    actual = simulate(predicted, closes, investment)
+    profitPotential = actual / maximal * 100
+
+    timespan = len(labels) / 24
+    percentage = actual / investment * 100
+    daily = actual / timespan
+
+    print "If the simulation lasts " + str(timespan) + " days and " + str(investment) + " euros are invested, then:"
+    print " - Total profit is: %0.2f euros." % actual
+    print " - Average daily profit is: %0.2f euros." % daily
+    print " - ROI is %0.2f%%." % percentage
+    print " - Profit is %0.2f%% of its \"potential\" with this betting strategy (if the model were to guess correctly every time, this value would be 100%%.)" % profitPotential
 
 print "Reading in data."
 
@@ -59,20 +63,12 @@ values = preprocessing.scale(data[:,0:data.shape[1] - 2])
 labels = data[:,data.shape[1] - 2]
 closes = data[:,data.shape[1] - 1]
 
-#classifier = svm.SVC()
-#classifier = RandomForestClassifier(n_estimators = 160)
-#classifier = GaussianNB()
-classifier = LogisticRegression()
-
 print "Selecting features."
 
-rfe = RFE(estimator = classifier, step = 1)
-rfe.fit(values, labels)
+pca = PCA(n_components = 2)
+selection = SelectKBest(k = 1)
+combined_features = FeatureUnion([("pca", pca), ("univ_select", selection)])
 
-removed = 0
-for i in range(len(rfe.support_)):
-    if not rfe.support_[i]:
-        values = numpy.delete(values, i - removed, 1)
-        removed += 1
+values = combined_features.fit(values, labels).transform(values)
 
-simulate(values, labels, closes)
+estimate(values, labels, closes)
